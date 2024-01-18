107fd07e79f49884fe025a37735b629d7633e80b
taufeeq-ahmed <themaestrotaufeeq@gmail.com>
2024-01-18 19:05:05 +0530

    data types added

---
+++ strings/other_data_types.sql
@@ -0,0 +1,76 @@
+-- char vs varchar 
+-- if cahr(12) vs varcahr(12) if we store 'hi' in char(12) sql will add whitespaces and make it length 12
+-- but varchar resizes and stores data in an efficient format .char is faster.
+
+
+
+-- for interes there are int ,tinyint,smallint,mediumint, bigint.
+
+
+-- for storing decimal numbers we have decimal(5,2) => means total 5 digits to store 2 will be after decimal , if u insert data that has more digits the number will be stored after truncating if the decimal digits are the ones that are more in number if the numbers before decimal are more in number then the db will thow a error float and double are also available in the same way
+
+-- dates times 
+
+CREATE TABLE people (
+	name VARCHAR(100),
+    birthdate DATE,
+    birthtime TIME,
+    birthdt DATETIME
+);
+ 
+INSERT INTO people (name, birthdate, birthtime, birthdt)
+VALUES ('Elton', '2000-12-25', '11:00:00', '2000-12-25 11:00:00');
+ 
+INSERT INTO people (name, birthdate, birthtime, birthdt)
+VALUES ('Lulu', '1985-04-11', '9:45:10', '1985-04-11 9:45:10');
+ 
+INSERT INTO people (name, birthdate, birthtime, birthdt)
+VALUES ('Juan', '2020-08-15', '23:59:00', '2020-08-15 23:59:00');
+
+
+SELECT CURTIME();
+ 
+SELECT CURDATE();
+ 
+SELECT NOW();
+ 
+INSERT INTO people (name, birthdate, birthtime, birthdt)
+VALUES ('Hazel', CURDATE(), CURTIME(), NOW());
+
+SELECT 
+    birthdate,
+    DAY(birthdate),
+    DAYOFWEEK(birthdate),
+    DAYOFYEAR(birthdate)
+FROM people;
+ 
+SELECT 
+    birthdate,
+    MONTHNAME(birthdate),
+    YEAR(birthdate)
+FROM people;
+
+SELECT 
+    birthtime,
+    HOUR(birthtime),
+    MINUTE(birthtime)
+FROM people;
+ 
+SELECT 
+    birthdt,
+    MONTH(birthdt),
+    DAY(birthdt),
+    HOUR(birthdt),
+    MINUTE(birthdt)
+FROM people;
+
+CREATE TABLE captions (
+  text VARCHAR(150),
+  created_at TIMESTAMP default CURRENT_TIMESTAMP
+);
+ 
+CREATE TABLE captions2 (
+  text VARCHAR(150),
+  created_at TIMESTAMP default CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
